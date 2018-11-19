4c63a04b99faf17670bf496954165f1c3428be93
Michael Becker <michael@bemit.codes>
2018-11-19 01:26:51 +0100

    rm react app;

--- react-app/src/App.js
+++
@@ -1,9 +0,0 @@
-import Loadable from 'react-loadable';
-import Loading from './page/Loading/Loading';
-
-const App = Loadable({
-    loader: () => import('./page/Base'),
-    loading: Loading,
-});
-
-export default App;
