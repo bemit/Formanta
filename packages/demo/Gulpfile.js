25362c4a3fc940aa48999e79d6624d9ba31c0abb
Michael Becker <michael@bemit.codes>
2022-03-18 01:22:47 +0100

    formanta-sass 0.21.1 prepare; demo upgrade cap

--- packages/demo/Gulpfile.js
+++ packages/demo/Gulpfile.js
@@ -1,45 +1,85 @@
-'use strict';
-const path = require('path');
-const {ampCreator} = require('create-amp-page');
+import path from 'path';
+import gulp from 'gulp';
+import {ampCreator, getPageInfo} from 'create-amp-page';
 
-module.exports = ampCreator({
-    port: 4488,
-    paths: {
-        styles: 'src/styles',
-        //stylesInject: 'main.css',
-        html: 'src/html',
-        htmlPages: 'src/html/pages',
-        media: 'src/media',
-        copy: [
-            {src: ['./src/api/*'], prefix: 1},
-            {src: ['./public/*'], prefix: 2},
-        ],
-        dist: 'build',
-        distMedia: 'media',
-        distStyles: 'styles',
+const port = process.env.PORT || 4488;
+const isDev = process.env.NODE_ENV === 'development';
+
+const urls = {
+    demo: {
+        local: {base: 'http://localhost:' + port + '/demo/'},
+        prod: {base: 'https://formanta.bemit.codes/'},
     },
-    twig: {
-        data: {
-            ampEnabled: false,
+};
+
+const pages = {
+    demo: {
+        paths: {
+            styles: 'src/styles',
+            stylesInject: 'main.css',
+            style: '*.scss',
+            html: 'src/html',
+            copy: [
+                {src: ['src/api/*'], prefix: 1},
+                {src: ['public/*'], prefix: 2},
+                {src: ['public/**/*'], prefix: 1},
+            ],
+            dist: 'build/demo',
+            distStyles: 'styles',
         },
-        fm: (file) => './src/data/' + path.basename(file).replace('.twig', '') + '.md',
-        fmMap: (data) => ({
-            head: {
-                title: data.attributes.title,
-                description: data.attributes.description,
-                lang: data.attributes.lang,
-            },
-        }),
+    },
+};
+
+// for infos check `create-amp-page` docs or typings/inline-doc!
+const tasks = ampCreator({
+    port: port,
+    dist: 'build',
+    srcMedia: 'src/media',
+    distMedia: 'media',
+    minifyHtml: !isDev,
+    cleanInlineCSS: !isDev,
+    cleanInlineCSSWhitelist: ['#toggle-inspect', '.demo'],
+    pages: pages,
+    collections: [{
+        //data: 'src/data',
+        fm: (file) => 'src/data/' + path.basename(file).slice(0, '.twig'.length * -1) + '.md',
+        tpl: 'src/html/pages/*.twig',
+        pagesByTpl: true,
+        base: '',
+        pageId: 'demo',
+    }],
+    cssInjectTag: '<style>',
+    twig: {
+        data: {},
+        fmMap: (data, files) => {
+            const pageId = files.pageId;
+            const pageEnv = isDev ? 'local' : 'prod';
+            const {
+                pagePath, pageBase, relPath,
+            } = getPageInfo(files, urls, pageId, pageEnv);
+            return {
+                pageId: pageId,
+                head: {
+                    title: data.attributes.title,
+                    description: data.attributes.description,
+                    lang: data.attributes.lang,
+                },
+                links: {
+                    canonical: pageBase + pagePath,
+                    origin: pageBase,
+                    cdn: isDev ? 'http://localhost:' + port + '/' : pageBase,
+                },
+            };
+        },
+        functions: [],
     },
     watchFolders: {
+        twig: ['src/data/**/*.json', 'src/data/**/*.md'],
         sass: [
             '../formantasass/**/*.scss',
         ],
+        media: [],
     },
-    ampOptimize: false,
-    minifyHtml: true,
-    cleanInlineCSS: false,
-    cleanInlineCSSWhitelist: ['#toggle-inspect', '.demo'],
-    //cssInjectTag: '<style>',
     prettyUrlExtensions: ['html'],
 });
+Object.keys(tasks).forEach(taskName => gulp.task(taskName, tasks[taskName]));
